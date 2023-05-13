2b915a5d60e0b4b65bc2b232c994cd55c22d2e11
Hermar <127502706+HermarJR@users.noreply.github.com>
2023-05-13 14:18:52 +0800

    Add project files.

---
+++ Nested for loop/Program.cs
@@ -0,0 +1,44 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+       bool TryAgain = true;
+       
+        while (TryAgain)
+        {
+
+            Console.Write("How many rows: ");
+            int rows = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("How many columns: ");
+            int columns = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("What symbol: ");
+            string symbol = Console.ReadLine();
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    Console.Write(symbol);
+                }
+                Console.WriteLine();
+            }
+
+            Console.Write("Would you like to continue? (Y/N) ");
+            string again = Console.ReadLine();
+            again = again.ToUpper();
+
+            if (again == "N")
+            {
+                TryAgain = false;
+                Console.WriteLine("Thank you for playing!");
+            }
+
+        }
+
+
+        Console.ReadKey();
+
+    }
+}
